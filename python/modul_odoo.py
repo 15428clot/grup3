c352c7a2a1fb76d295a871d5b8ac01ac2f466081
grup3 <caroga@inf1-caroga.fjeclot.net>
2017-03-16 02:35:11 +0100

    Càrrega de tots els arxius

---
+++ python/modul_odoo.py
@@ -0,0 +1,36 @@
+"""
+Una calculadora d'interés compost que a partir del capital inicial, el tipus d'interés anual, el temps en anys
+i la retenció aplicada, mostri els capital final obtingut, els interessos generats i la retenció aplicada
+
+Interés compuesto es el que resulta cuando los intereses producidos por un capital en cada periodo se agregan al capital
+para calcular los intereses del periodo siguiente.
+Con esta aplicación podemos calcular el capital inicial, el capital final, el tiempo, el tipo de interés o los intereses
+de una operación de interés compuesto.
+
+Ejemplo: Calcular el valor de los intereses que tenemos que pagar para devolver 100.000 euros al 8,65% anual durante 15 años.
+Resultado: 247.093,0837 euros
+
+"""
+
+
+
+class Calculadora:
+
+    def __init__(self):
+
+        # Variables de clase
+        self.capital_inicial = None
+        self.capital_final = None
+        self.interes = None
+        self.temps = None
+        self.retencio = None
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    # Creacio de l'objecte
+    calc = Calculadora()
